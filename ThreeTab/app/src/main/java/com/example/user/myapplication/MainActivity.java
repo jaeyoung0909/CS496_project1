1da5c8980140f2683071c9cf5e04c73938864f4b
Jaeyoung <jaeyoung0909@kaist.ac.kr>
2017-12-27 10:51:39 +0900

    add jaeyoung's a, b, c tab

--- ThreeTab/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ ThreeTab/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void Button_number(View view) {
+        Intent intent = new Intent(this, PhoneNumber.class);
+        startActivity(intent);
+
+    }
+
+    public void Button_picture(View view) {
+        Intent intent = new Intent(this, Picture.class);
+        startActivity(intent);
+
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.textView);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
