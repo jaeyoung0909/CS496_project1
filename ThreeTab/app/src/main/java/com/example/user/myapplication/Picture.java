559d7fd3663498c6481e393abe2c778d007bf686
Jaeyoung <jaeyoung0909@kaist.ac.kr>
2017-12-27 22:08:07 +0900

    jaeyoung's B part

--- ThreeTab/app/src/main/java/com/example/user/myapplication/Picture.java
+++ ThreeTab/app/src/main/java/com/example/user/myapplication/Picture.java
@@ -1,13 +1,175 @@
 package com.example.user.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
-public class Picture extends AppCompatActivity {
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 
+public class Picture extends AppCompatActivity implements View.OnClickListener{
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    private Context mContext;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_iMAGE = 2;
+
+    private Uri mlmageCaptureUri;
+    private String absolutePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        GridView gridview  = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parents, View v, int position, long id){
+
+                Toast.makeText(Picture.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button btn_agreeJoin = (Button)this.findViewById(R.id.button3);
+
+        btn_agreeJoin.setOnClickListener(this);
     }
+
+    public void doTakeAlbumAction(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK) return;
+        switch(requestCode){
+            case PICK_FROM_ALBUM :{
+                mlmageCaptureUri = data.getData();
+                Log.d("SmartWhell", mlmageCaptureUri.getPath().toString());
+            }
+            case CROP_FROM_iMAGE:{
+                if(resultCode != RESULT_OK){
+                    return;
+                }
+
+                final Bundle extras = data.getExtras();
+
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SmartWheel/" + System.currentTimeMillis() + ".jpg";
+
+                if(extras != null){
+                    Bitmap photo = extras.getParcelable("data");
+
+                    absolutePath = filePath;
+                    break;
+                }
+
+                File f = new File(mlmageCaptureUri.getPath());
+                if(f.exists())  f.delete();
+            }
+        }
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath){
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
+
+        if(!directory_SmartWheel.exists())  directory_SmartWheel.mkdir();
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
+            out.flush();
+            out.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ImageView imageView;
+            if(view == null){
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER);
+                imageView.setPadding(8, 8, 8, 8);
+            } else{
+                imageView = (ImageView) view;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+    }
+
+
 }
